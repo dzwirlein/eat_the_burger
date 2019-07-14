9b91038f6fcad64aa93be2a6f11f3468343a6dea
dzwirlein <47755855+dzwirlein@users.noreply.github.com>
2019-07-13 22:35:29 -0500

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,10 @@
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+}else {
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,6 +13,8 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+};
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -15,5 +22,7 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
+
 
 module.exports = connection;
